b1e091f11dc3d33310f0b0742e4edb6a1d9e5951
Ayuan <838478249@qq.com>
2022-11-08 09:47:42 +0800

    业务用demo

---
+++ src/main/java/leetcode/editor/cn/CountConsistentStrings.java
@@ -0,0 +1,34 @@
+package leetcode.editor.cn;
+
+import com.sun.deploy.util.StringUtils;
+
+/**
+ * @author Ayuan
+ * @Description: 1684. 统计一致字符串的数目
+ * @date 2022/11/8 09:26
+ */
+public class CountConsistentStrings {
+
+    class Solution {
+        public int countConsistentStrings(String allowed, String[] words) {
+            int[] map = new int[26];
+            for (int i = 0; i < allowed.length(); i++) {
+                map[allowed.charAt(i) - 'a']++;
+            }
+            int res = 0;
+            for (String w : words) {
+                boolean isConsistent = true;
+                for (int i = 0; i < w.length(); i++) {
+                    if (map[w.charAt(i) - 'a'] == 0) {
+                        isConsistent = false;
+                        break;
+                    }
+                }
+                if (isConsistent) {
+                    res++;
+                }
+            }
+            return res;
+        }
+    }
+}
