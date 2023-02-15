42f5d765b017ebfe7c82a178391a6ff6ad801935
Ayuan <838478249@qq.com>
2023-02-15 10:10:52 +0800

    feat:每日一题

--- src/main/java/leetcode/editor/cn/IsGoodArray.java
+++ src/main/java/leetcode/editor/cn/IsGoodArray.java
@@ -1,24 +1,20 @@
 package leetcode.editor.cn;
 
+import java.util.Arrays;
+
 /**
  * @author Ayuan
- * @Description: TODO
+ * @Description: 1250. 检查「好数组」
  * @date 2023/2/15 10:07
  */
 public class IsGoodArray {
     class Solution {
         public boolean isGoodArray(int[] nums) {
-            int res = nums[0];
-            for (int num : nums) {
-                res = gcd(res, num);
-                if (res == 1) break;
-            }
-            return res == 1;
+            return Arrays.stream(nums).reduce(this::gcd).getAsInt() == 1;
         }
 
         private int gcd(int a, int b) {
             if (b == 0) return a;
-            if (b > a) return gcd(b, a);
             return gcd(b, a % b);
         }
     }
