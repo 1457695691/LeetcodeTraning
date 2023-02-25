915cd7e3c6cf58d528b3376f5f0d5972f9a63286
Ayuan <838478249@qq.com>
2023-02-25 10:04:31 +0800

    feat:每日一题

---
+++ src/main/java/leetcode/editor/cn/MinimumSwap.java
@@ -0,0 +1,30 @@
+package leetcode.editor.cn;
+
+/**
+ * @author Ayuan
+ * @Description: 1247. 交换字符使得字符串相同
+ * @date 2023/2/25 09:47
+ */
+public class MinimumSwap {
+
+    public static void main(String[] args) {
+        String s1 = "xyxyyx"; //101001
+        String s2 = "yxyxxy"; //010110
+        MinimumSwap.Solution solution = new MinimumSwap().new Solution();
+        int out = solution.minimumSwap(s1, s2);
+        System.out.println(out);
+    }
+
+    class Solution {
+        public int minimumSwap(String s1, String s2) {
+            int[] cnt = new int[2];
+            for (int i = 0; i < s1.length(); ++i) {
+                if (s1.toCharArray()[i] != s2.toCharArray()[i]) {
+                    ++cnt[s1.toCharArray()[i] % 2];
+                }
+            }
+            int d = cnt[0] + cnt[1];
+            return d % 2 != 0 ? -1 : d / 2 + cnt[0] % 2;
+        }
+    }
+}
