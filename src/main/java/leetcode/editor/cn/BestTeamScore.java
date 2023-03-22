f913515a18e3c501dad31b5ece8be272af45484c
Ayuan <838478249@qq.com>
2023-03-22 11:18:10 +0800

    feat:每日一题

--- src/main/java/leetcode/editor/cn/BestTeamScore.java
+++ src/main/java/leetcode/editor/cn/BestTeamScore.java
@@ -1,5 +1,7 @@
 package leetcode.editor.cn;
 
+import java.util.Arrays;
+
 /**
  * @author Ayuan
  * @Description: 1626. 无矛盾的最佳球队
@@ -9,7 +11,24 @@ public class BestTeamScore {
 
     class Solution {
         public int bestTeamScore(int[] scores, int[] ages) {
-            return 0;
+            int n = scores.length, maximum = 0;
+            int[][] candidate = new int[n][2];
+            for (int i = 0; i < n; i++) {
+                candidate[i][0] = ages[i];
+                candidate[i][1] = scores[i];
+            }
+            Arrays.sort(candidate, (a, b) -> a[0] == b[0] ? b[1] - a[1] : b[0] - a[0]);
+            int[] dp = new int[n];
+            for (int i = 0; i < n; i++) {
+                dp[i] = candidate[i][1];
+                for (int j = 0; j < i; j++) {
+                    if (candidate[i][1] <= candidate[j][1]) {
+                        dp[i] = Math.max(dp[i], dp[j] + candidate[i][1]);
+                    }
+                }
+                maximum = Math.max(maximum, dp[i]);
+            }
+            return maximum;
         }
     }
 }
