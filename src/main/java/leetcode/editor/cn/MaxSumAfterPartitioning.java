4565179a4fa51e1e959c905ed9efbef46eda2e13
Ayuan <838478249@qq.com>
2023-04-19 14:19:25 +0800

    feat:每日一题

--- src/main/java/leetcode/editor/cn/MaxSumAfterPartitioning.java
+++ src/main/java/leetcode/editor/cn/MaxSumAfterPartitioning.java
@@ -1,5 +1,7 @@
 package leetcode.editor.cn;
 
+import static java.lang.Math.max;
+
 /**
  * @author Ayuan
  * @Description: 1043. 分隔数组以得到最大和
@@ -12,8 +14,8 @@ public class MaxSumAfterPartitioning {
             int[] f = new int[n + 1];
             for (int i = 0; i < n; ++i) {
                 for (int j = i, mx = 0; j > i - k && j >= 0; --j) {
-                    mx = Math.max(mx, arr[j]);
-                    f[i + 1] = Math.max(f[i + 1], f[j] + (i - j + 1) * mx);
+                    mx = max(mx, arr[j]);
+                    f[i + 1] = max(f[i + 1], f[j] + (i - j + 1) * mx);
                 }
             }
             return f[n];
