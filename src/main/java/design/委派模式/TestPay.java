f1db1e54cb7ff1c2ef98d81cf023618469fc2794
Ayuan <838478249@qq.com>
2023-07-20 17:49:25 +0800

    feat:每日一题

---
+++ src/main/java/design/委派模式/TestPay.java
@@ -0,0 +1,12 @@
+package design.委派模式;
+
+/**
+ * @author Ayuan
+ * @Description: 委派模式
+ * @date 2023/7/20 15:20
+ */
+public class TestPay {
+    public static void main(String[] args) {
+
+    }
+}
