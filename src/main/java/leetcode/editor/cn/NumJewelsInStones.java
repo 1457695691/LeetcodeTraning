54f2c9cdde1af29c3a0b3f805c2bce2dcf3311ca
Ayuan <838478249@qq.com>
2023-07-24 10:47:20 +0800

    feat:每日一题

---
+++ src/main/java/leetcode/editor/cn/NumJewelsInStones.java
@@ -0,0 +1,21 @@
+package leetcode.editor.cn;
+
+/**
+ * @author Ayuan
+ * @Description: 771. 宝石与石头
+ * @date 2023/7/24 10:40
+ */
+public class NumJewelsInStones {
+
+    class Solution {
+        public int numJewelsInStones(String J, String S) {
+            int count = 0;
+            for (int i = 0; i < S.length(); i++) {
+                if (J.contains(S.charAt(i) + "")) {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
